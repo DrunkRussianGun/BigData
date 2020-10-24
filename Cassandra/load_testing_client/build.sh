20ac010b5f6181659ffd9e8effe65b5bff441352
Drunk Russian Gun <drunkrussiangun@gmail.com>
2020-10-24 12:15:54 +0300

    Добавил комментарий в скрипт сборки клиента для нагрузочного тестирования

--- Cassandra/load_testing_client/build.sh
+++ Cassandra/load_testing_client/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-output=$1
+outputPex=$1
+
+# Для Linux можно использовать платформу linux-x86_64-cp-38-cp38
 platform=$2
 
-rm -f "$output"
+rm -f "$outputPex"
 
-pex . -v -c main.py -r requirements.txt -o "$output" --platform "$platform"
+pex . -v -c main.py -r requirements.txt -o "$outputPex" --platform "$platform"
 
-read -p "Press enter to continue"
+read -p "Press enter to exit"
